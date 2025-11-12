26e0fd44bc0966128241fe277eacf7422dc93723
Angelitoow <barraganangelmauricio@gmail.com>
2025-11-11 21:26:38 -0500

    puerto

--- Backend/index.js
+++ Backend/index.js
@@ -12,10 +12,10 @@ app.use(express.json());
 
 const db = mysql.createConnection({
   host: process.env.MYSQLHOST,
-  port: process.env.MYSQLPORT,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT || 3306
 });
 
 /*------------------------------------------CORS------------------------------------------*/
